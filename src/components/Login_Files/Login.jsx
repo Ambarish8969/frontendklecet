32017f3a53c81a82ed71dcc2d8de5d2185c82e33
Ambarish <ambarish8653@gmail.com>
2021-07-08 20:45:08 +0530

    login file created

---
+++ src/components/Login_Files/Login.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./Login.css";
+
+function Login() {
+    
+    return (
+        <>
+            <div id="login-page">
+                <h1 className="clg-name-login">KLE COLLEGE OF ENGINEERING AND TECHNOLOGY, CHIKODI</h1>
+                <div className="login-card-middle">
+                    <div className="login-card">
+                        <form action="">
+                            <h3>LOGIN</h3>
+                            <input type="email" placeholder="Email" required className="mailid" /> <br /><br />
+                            <input type="text" placeholder="USN" className="usnid" required /><br /><br />
+                            <input type="password" placeholder="Password" required className="passwordid" /> <br /> <br />
+                            <input type="submit" value="Login" />
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Login;
