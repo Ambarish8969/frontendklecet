12add9383ccd623ac5a9c720f07052ba7614c855
Ambarish <ambarish8653@gmail.com>
2021-07-09 22:24:26 +0530

    updated

--- src/components/About_Files/About.js
+++ src/components/About_Files/About.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const About = () => {
     return (
-        <h1>About page</h1>
+        <div>
+            <h1>About page</h1>
+        </div>
     )
 }
 export default About;
